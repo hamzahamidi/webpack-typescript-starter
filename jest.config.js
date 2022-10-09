098f414dc32fe681b24a68908ab1e3ac3956a30b
Hamza Hamidi <22576950+hamzahamidi@users.noreply.github.com>
2022-10-09 15:43:21 +0000

    build(deps): Update deps

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  roots: [
-    "<rootDir>/src"
-  ],
-  transform: {
-    ".(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest-config/file-mock.js",
-    '.(css|less)$': '<rootDir>/jest-config/style-mock.js'
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['src/components/**/*.{js,jsx,ts,tsx}'],
+  moduleNameMapper: {
+    '.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/jest-config/file-mock.js',
+    '\\.(css|scss|sass|less)$': 'identity-obj-proxy',
   },
 };
